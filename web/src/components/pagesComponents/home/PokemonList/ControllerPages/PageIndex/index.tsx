548cf2718538674f0582b5021ee7229359ce86b7
brunoamaia <brunoamaia94@gmail.com>
2021-07-11 01:50:38 -0300

    fix menu of pgination

--- web/src/components/pagesComponents/home/PokemonList/ControllerPages/PageIndex/index.tsx
+++ web/src/components/pagesComponents/home/PokemonList/ControllerPages/PageIndex/index.tsx
@@ -28,7 +28,7 @@ export function PageIndex() {
 
   return (
     <PageIndexStyles>
-      {pageActually < 6 && (
+      {pageActually < 5 && (
         <>
           <button onClick={() => handleChangePage(offsets[0])}>1</button>
           <button onClick={() => handleChangePage(offsets[1])}>2</button>
@@ -42,7 +42,7 @@ export function PageIndex() {
         </>
       )}
 
-      {pageActually >= 6 && pageActually < quantityPages - 5 && (
+      {pageActually >= 5 && quantityPages - pageActually >= 4 && (
         <>
           <button onClick={() => handleChangePage(offsets[0])}>1</button>
           <button>...</button>
@@ -56,6 +56,28 @@ export function PageIndex() {
             {pageActually + 1}
           </button>
           <button>...</button>
+          <button onClick={() => handleChangePage(offsets[quantityPages - 1])}>
+            {quantityPages}
+          </button>
+        </>
+      )}
+
+      {pageActually >= 5 && quantityPages - pageActually < 4 && (
+        <>
+          <button onClick={() => handleChangePage(offsets[0])}>1</button>
+          <button>...</button>
+          <button onClick={() => handleChangePage(offsets[quantityPages - 5])}>
+            {quantityPages - 4}
+          </button>
+          <button onClick={() => handleChangePage(offsets[quantityPages - 4])}>
+            {quantityPages - 3}
+          </button>
+          <button onClick={() => handleChangePage(offsets[quantityPages - 3])}>
+            {quantityPages - 2}
+          </button>
+          <button onClick={() => handleChangePage(offsets[quantityPages - 2])}>
+            {quantityPages - 1}
+          </button>
           <button onClick={() => handleChangePage(offsets[quantityPages - 1])}>
             {quantityPages}
           </button>
